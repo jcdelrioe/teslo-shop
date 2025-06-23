375a9ecceb0cbb3bea7b64368a30f2f7304c9486
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-23 08:29:30 -0500

    config env variables, update README

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Importante para que esté disponible en todo el proyecto
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      database: process.env.DB_NAME,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
