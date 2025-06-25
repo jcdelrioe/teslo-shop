71f3e883fec0d4a2c5e4e81614c45b9be4c1d7f4
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-25 17:45:16 -0500

    Return secureUrl

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -14,10 +14,14 @@ import { Response } from 'express'
 
 import { FilesService } from './files.service'
 import { fileFilter, fileNamer } from './helpers'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -47,7 +51,8 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image')
     }
 
-    const secureUrl = `${file.filename}`
+    // const secureUrl = `${file.filename}`
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`
 
     return { secureUrl }
   }
