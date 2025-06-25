7ca4e0acc4a375acec2e531b4b7ba04993db01f6
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-25 10:54:50 -0500

    update images

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,7 +5,7 @@ import {
   NotFoundException,
 } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { DataSource, Repository } from 'typeorm'
 
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
@@ -25,6 +25,8 @@ export class ProductsService {
 
     @InjectRepository(ProductImage)
     private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSoruce: DataSource,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
@@ -93,18 +95,33 @@ export class ProductsService {
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
-    })
+    const { images, ...toUpdate } = updateProductDto
+    const product = await this.productRepository.preload({ id, ...toUpdate })
 
     if (!product) throw new NotFoundException(`Product with id ${id} not found`)
 
+    const queryRunner = this.dataSoruce.createQueryRunner()
+    await queryRunner.connect()
+    await queryRunner.startTransaction()
+
     try {
-      await this.productRepository.save(product)
-      return product
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } })
+
+        product.images = images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        )
+      }
+
+      await queryRunner.manager.save(product)
+      // await this.productRepository.save(product)
+      await queryRunner.commitTransaction()
+      await queryRunner.release()
+
+      return this.findOnePlain(id)
     } catch (error) {
+      await queryRunner.rollbackTransaction()
+      await queryRunner.release()
       this.handleDBExceptions(error)
     }
   }
