e0c6b7d681afc73b69cd3f6da0eaac7168d1e394
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-25 11:56:16 -0500

    Seed service, update README

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { ProductsService } from './../products/products.service'
+import { initialData } from './data/seed-data'
 
 @Injectable()
 export class SeedService {
@@ -12,6 +13,16 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productService.deleteAllProducts()
+
+    const products = initialData.products
+
+    const insertPromises: Promise<any>[] = []
+
+    products.forEach((product) => {
+      insertPromises.push(this.productService.create(product))
+    })
+
+    await Promise.all(insertPromises)
 
     return true
   }
