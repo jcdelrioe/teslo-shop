d3ff23bfede7cee3a6b9580c9d3b573bce26872c
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-06-25 11:33:15 -0500

    import export modules, delete all products

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common'
+import { ProductsService } from './../products/products.service'
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productService: ProductsService) {}
+
+  async runSeed() {
+    await this.insertNewProducts()
+    return 'Seed executed successfully'
+  }
+
+  private async insertNewProducts() {
+    await this.productService.deleteAllProducts()
+
+    return true
+  }
+}
