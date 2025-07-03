c42f00dac45f3dfbd62d80b8e363a557d570434b
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-07-03 10:54:33 -0500

    install gateway, socket.io config

---
+++ src/message-ws/message-ws.gateway.ts
@@ -0,0 +1,20 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets'
+import { MessageWsService } from './message-ws.service'
+import { Socket } from 'socket.io'
+
+@WebSocketGateway({ cors: true })
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messageWsService: MessageWsService) {}
+  handleConnection(client: Socket) {
+    console.log('Cliente conectado: ', client.id)
+  }
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado: ', client.id)
+  }
+}
